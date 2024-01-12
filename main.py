054ff908e8d11c70a879fb47beea7e39567d15eb
Soulbolt <soulbolt@gmail.com>
2024-01-11 23:11:36 -0700

    First commit, base files, flask code to start service, route to capture user input from html form to validate data.

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    return f"<h1>Name: {request.form.get("username")}, Password: {request.form.get("password")}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
